d4e7e8df26fcfbbdbd8e18f811244aa33555b6d2
YuanhongShi <31866504+YuanhongShi@users.noreply.github.com>
2017-09-29 19:18:36 -0400

    version 1

--- PS01/index.js
+++ PS01/index.js
@@ -1,3 +1,74 @@
 var svg = d3.select('svg');
 
 /* Your code goes here */
+
+//myArray = [];
+
+/*
+for (i = 0; i < 30; i++){
+    var seta = 2 * Math.PI / 30 * i ;
+    //console.log(seta);
+    var object = {cx: 250 + 200 * Math.cos(seta), cy: 250 + 200 * Math.sin(seta), r: 10};
+
+    myArray.push(object);
+    //console.log(myArray[i].cx);
+}
+*/
+
+function circle_ring(myArray, i, j, r){
+
+    for (j = 0; j < 5; j++){
+        r = 20 * j;
+        for (i = 0; i < 30; i++){
+            var seta = 2 * Math.PI / 30 * i ;
+            //console.log(seta);
+            var object = {cx: 250 + (50 + r) * Math.cos(seta), cy: 250 + (50 + r) * Math.sin(seta), r: 5 + j};
+
+            myArray.push(object);
+            //console.log(myArray[i].cx);
+        }
+
+    }
+
+    return myArray;
+}
+
+myArray = [];
+var i;
+var j;
+var r;
+
+circle_ring(myArray, i, j, r);
+
+console.log(myArray);
+
+svg.append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', 500)
+    .attr('height', 500)
+    .attr('fill', 'white')
+    .attr('stroke', 'pink')
+    .attr('stroke-width', 3);
+
+svg.selectAll('circle')
+    .data(myArray)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d){
+        console.log(d.cx);
+        return d.cx;
+    })
+    .attr('cy', function(d){
+        return d.cy;
+    })
+    .attr('r', function(d){
+        return d.r;
+    })
+    .attr('fill', 'blue');
+
+svg.append('circle')
+    .attr('cx', 250)
+    .attr('cy', 250)
+    .attr('r', 15)
+    .attr('fill', 'pink');
--- generateData/index.js
+++ generateData/index.js
@@ -1,3 +1,133 @@
 var svg = d3.select('svg');
 
 /* Your code goes here */
+/*var arrary =["milk", "egg", "bread", "butter"];
+
+console.log(arrary[0]);
+
+var student = {"name": "Erica", "job": "professor", "age": 34 };
+
+console.log(student.age);
+
+var classRoom = [{"name": "Erica1", "job": "professor", "age": 34 },
+    {"name": "Erica2", "job": "professor", "age": 34 },
+    {"name": "Erica3", "job": "professor", "age": 34 }
+];
+
+console.log(classRoom);
+
+console.log(classRoom[1]);
+
+console.log(classRoom[1].age);*/
+
+//Mathod1: manualy create the data
+
+/*myCircle = {"cx": 100, "cy": 200, "r": 20, "color": 'blue'};
+
+var myArray = [
+    myCircle,
+    {"cx": 40, "cy": 200, "r": 20, "color": 'blue'},
+    {"cx": 60, "cy": 200, "r": 20, "color": 'blue'},
+    {"cx": 80, "cy": 200, "r": 20, "color": 'blue'},
+    {"cx": 100, "cy": 200, "r": 20, "color": 'blue'}
+];
+
+svg.selectAll('circle')
+    .data(myArray)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d){
+        console.log(d.cx);
+        return d.cx;
+    })
+    .attr('cy', function(d){
+        return d.cy;
+    })
+    .attr('r', function(d){
+        return d.r;
+    })
+    .attr('fill', function(d){
+        return d.color;
+    });
+*/
+
+
+//Mathod2: create the data by loop or forEach
+
+/*myArray =[{}, {}, {}, {}, {}];
+
+for (i = 0; i < 5; i++){
+    //console.log('circle' + i);
+    //myArray[i].name = 'circle'+ i;
+    myArray[i].cx = Math.random() * 100; //random function from the math library
+    myArray[i].cy = i * 100;
+    myArray[i].r = 10 + i * 10;
+
+}
+
+console.log(myArray);
+
+svg.selectAll('circle')
+    .data(myArray)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d){
+        console.log(d.cx);
+        return d.cx;
+    })
+    .attr('cy', function(d){
+        return d.cy;
+    })
+    .attr('r', function(d){
+        return d.r;
+    })
+    .attr('fill', function(d){
+        return d.color;
+    })
+    .attr('fill', 'blue');
+
+*/
+
+/*myArray =[{}, {}, {}, {}, {}];
+
+myArray.forEach(function (element,i){
+    element.cx = i * 100;
+    element.cy = i * 100;
+    element.r = 25;
+});
+
+console.log(myArray);
+*/
+
+
+//Mathod3: create the data from a empty array
+
+myArray = [];
+
+for (i = 0; i < 100; i++){
+    var object = {cx: 400 * Math.random(), cy: 400 * Math.random(), r: 10};
+    myArray.push(object);
+}
+
+console.log(myArray);
+
+
+svg.selectAll('circle')
+    .data(myArray)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d){
+        console.log(d.cx);
+        return d.cx;
+    })
+    .attr('cy', function(d){
+        return d.cy;
+    })
+    .attr('r', function(d){
+        return d.r;
+    })
+    .attr('fill', function(d){
+        return d.color;
+    })
+    .attr('fill', 'blue');
+
