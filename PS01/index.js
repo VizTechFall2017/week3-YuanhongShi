2fa811783503da3498314eada6e9bce6dbf5a9e2
YuanhongShi <31866504+YuanhongShi@users.noreply.github.com>
2017-09-29 20:13:27 -0400

    Version 2

--- PS01/index.js
+++ PS01/index.js
@@ -4,17 +4,7 @@ var svg = d3.select('svg');
 
 //myArray = [];
 
-/*
-for (i = 0; i < 30; i++){
-    var seta = 2 * Math.PI / 30 * i ;
-    //console.log(seta);
-    var object = {cx: 250 + 200 * Math.cos(seta), cy: 250 + 200 * Math.sin(seta), r: 10};
-
-    myArray.push(object);
-    //console.log(myArray[i].cx);
-}
-*/
-
+/*Version_01
 function circle_ring(myArray, i, j, r){
 
     for (j = 0; j < 10; j++){
@@ -65,10 +55,79 @@ svg.selectAll('circle')
     .attr('r', function(d){
         return d.r;
     })
-    .attr('fill', 'blue');
+    .attr('fill', '#344150');
 
 svg.append('circle')
     .attr('cx', 250)
     .attr('cy', 250)
     .attr('r', 15)
     .attr('fill', 'pink');
+
+*/
+
+//Version_02
+
+function circle_ring(myArray, a, i, j, r, color){
+
+    for (j = 0; j < 10; j++){
+        r = 20 * j;
+        for (i = 0; i < 20; i++){
+            var seta = (a + 360 / 20 * i) / 180 * Math.PI;
+            var object = {cx: 250 + (50 + r) * Math.cos(seta), cy: 250 + (50 + r) * Math.sin(seta), r:  2 + j, fill: color};
+
+            myArray.push(object);
+
+        }
+
+    }
+
+    return myArray;
+}
+
+myArray1 = [];
+myArray2 = [];
+myArray = [];
+var i;
+var j;
+var r;
+
+circle_ring(myArray1, 0, i, j, r, '#F9F649');
+circle_ring(myArray2, -3, i, j, r, 'black');
+
+myArray = myArray2.concat(myArray1);
+
+svg.append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', 500)
+    .attr('height', 500)
+    .attr('fill', 'white')
+    .attr('stroke', '#F9F649')
+    .attr('stroke-width', 8);
+
+
+svg.selectAll('circle')
+    .data(myArray)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d){
+
+        return d.cx;
+    })
+    .attr('cy', function(d){
+        return d.cy;
+    })
+    .attr('r', function(d){
+        return d.r;
+    })
+    .attr('fill', function(d){
+        return d.fill;
+    });
+
+
+
+svg.append('circle')
+    .attr('cx', 250)
+    .attr('cy', 250)
+    .attr('r', 15)
+    .attr('fill', '#F9F649');
