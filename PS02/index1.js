4ee8095b522246ea499f0caed671a67f4b3e0d48
YuanhongShi <31866504+YuanhongShi@users.noreply.github.com>
2017-10-01 21:25:48 -0400

    PS02_check data

--- PS02/index1.js
+++ PS02/index1.js
@@ -3,14 +3,14 @@ var svg = d3.select('svg');
 /* Your code goes here */
 
 d3.csv('./data.csv', function(dataIn){
-    console.log(dataIn);
+
 
     svg.selectAll('circle')
         .data(dataIn)
         .enter()
         .append('circle')
         .attr('cx', function(d){
-            console.log(d.x);
+            //console.log(d.x);
             return d.x;
         })
         .attr('cy', function(d){
@@ -21,5 +21,6 @@ d3.csv('./data.csv', function(dataIn){
         })
         .attr('fill', function(d){
             return d.fill;
+            console.log(d.fill);
         });
 });
